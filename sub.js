6088776216c944ec9c5fe73793cd73a2540a6737
Pier-Alexandre Trudeau <pier-alexandretrudeau@hotmail.ca>
2020-01-26 14:30:52 -0500

    Format message with a clean JSON

--- sub.js
+++ sub.js
@@ -1,12 +1,19 @@
 // MQTT Subcriber
 var mqtt = require('mqtt')
-var client = mqtt.connect('mqtt://localhost:1883');
+var client = mqtt.connect('mqtt://mqtt.cgmu.io:1883');
 var myArgs = process.argv.slice(2)
-var topic = myArgs[0]
+var topic = "#"//"worldcongress2017/pilot_resologi"// + myArgs[0]
 
 client.on('message', (topic, message) => {
     message = message.toString()
-    console.log(message)
+    var stringBuf = message && message.toString('utf-8')
+    try {
+      var json = JSON.parse(stringBuf);
+       console.log(json);
+    } 
+    catch (e) {
+      console.error(stringBuf);
+    }
 })
 
 client.on('connect', ()=>{
