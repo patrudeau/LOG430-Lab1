86ca026a3e0ac8a4aa2da978adb022f755238918
Thamer2018 <thameur.aissaoui@gmail.com>
2020-03-26 15:49:51 -0400

    clean up

--- Subscriber/publisherSubscriber.js
+++ Subscriber/publisherSubscriber.js
@@ -1,12 +1,28 @@
-// MQTT publisher
+// MQTT Subcriber
 var mqtt = require('mqtt')
-var client = mqtt.connect('mqtt://localhost:1883')
-var topic = 'trafic'
-var message = 'Hello World!'
+var client = mqtt.connect('mqtt://mqtt.cgmu.io:1883');
+var myArgs = process.argv.slice(2)
+var topic = "worldcongress2017/pilot_resologi/#"// + myArgs[0]
+
+client.on('message', (topic, message) => {
+    message = message.toString()
+    var stringBuf = message && message.toString('utf-8')
+    try {
+      var json = JSON.parse(stringBuf);
+       console.log(json);
+    } 
+    catch (e) {
+      console.error(stringBuf);
+    }
+})
 
 client.on('connect', ()=>{
-    setInterval(()=>{
-        client.publish(topic, message)
-        console.log('Message sent!', message)
-    }, 5000)
+    client.subscribe(topic)
+    console.log("connected")
+    console.log("subscribe to topic -> " + topic)
+    setTimeout(function(){ client.end() }, 3000);
+})
+
+client.on('close', ()=>{
+    console.log("Connection Closed")
 })
